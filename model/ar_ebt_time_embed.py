023ff150e1ea4a871512f2951146f77c571e123d
Arttish <super.art-tish@yandex.ru>
2026-04-10 20:19:06 +0300

    fix seqlen:2-1

--- model/ar_ebt_time_embed.py
+++ model/ar_ebt_time_embed.py
@@ -416,7 +416,7 @@ class Attention(nn.Module):
         if freqs_cis.shape[0] != original_seqlen + 1:
             extra_needed = original_seqlen + 1 - freqs_cis.shape[0]
             extra_freqs = precompute_freqs_cis(
-                dim=self.args.dim // self.args.n_heads,
+                dim=freqs_cis.dim,
                 end=extra_needed
             )
             freqs_cis = torch.cat([freqs_cis, extra_freqs.to(freqs_cis.device)], dim=0)
